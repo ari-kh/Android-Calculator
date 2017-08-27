8bf0506f7c5c94a8fae4a3f455c615cd2e5f328f
Ari Khuyagbaatar <ari.cnd@gmail.com>
2017-08-27 22:25:22 +0800

    pre-task

--- Calculator/app/src/main/java/com/ubclaunchpad/calculator/CalculatorActivity.java
+++ Calculator/app/src/main/java/com/ubclaunchpad/calculator/CalculatorActivity.java
@@ -6,13 +6,43 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
+import java.lang.*;
+
 public class CalculatorActivity extends AppCompatActivity implements View.OnClickListener{
     private final static String TAG = CalculatorActivity.class.getSimpleName();
 
+    private EditText inputOne;
+    private EditText inputTwo;
+    private Button add;
+    private Button sub;
+    private Button mul;
+    private Button div;
+    private Button pow;
+
+
     @Override
+
+    // initializing the activity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calculator);
+
+        // retrieving inputs
+        inputOne = (EditText) findViewById(R.id.firstInput);
+        inputTwo = (EditText) findViewById(R.id.secondInput);
+        add = (Button) findViewById(R.id.operation_add);
+        sub = (Button) findViewById(R.id.operation_sub);
+        mul = (Button) findViewById(R.id.operatio_multi);
+        div = (Button) findViewById(R.id.operation_div);
+        pow = (Button) findViewById(R.id.operation_pow);
+
+        // click listeners
+        add.setOnClickListener(this);
+        sub.setOnClickListener(this);
+        mul.setOnClickListener(this);
+        div.setOnClickListener(this);
+        pow.setOnClickListener(this);
+
     }
 
     /**
@@ -23,29 +53,46 @@ public class CalculatorActivity extends AppCompatActivity implements View.OnClic
      */
     @Override
     public void onClick(View v) {
+
+        float firstInput = 0;
+        float secondInput = 0;
+        float result = 0;
+
+        if (TextUtils.isEmpty(inputOne.getText().toString()) || TextUtils.isEmpty(inputTwo.getText().toString())) {
+            return;
+        }
+        firstInput = Float.parseFloat(inputOne.getText().toString());
+        secondInput = Float.parseFloat(inputTwo.getText().toString());
+
+
         switch (v.getId())
         {
             case R.id.operation_add:
             {
-                //TODO add function
+                result = firstInput + secondInput;git
                 break;
             }
             case R.id.operation_sub:
             {
-                //TODO subtract function
+                result = firstInput - secondInput;
                 break;
             }
             case R.id.operation_multi:
             {
-                //TODO multiply function
+                result = firstInput * secondInput;
                 break;
             }
             case R.id.operation_div:
             {
-                //TODO divide function
+                result = firstInput / secondInput;
+                 break;
+            }
+            // optional operation (power)
+            case R.id.operation_pow:
+            {
+                result = (float) Math.power(firstInput, secondInput);
                 break;
             }
-            //TODO any extra implmentations (optional)
             default:
             {
                 Toast.makeText(this, "Click not implmented yet", Toast.LENGTH_LONG).show();
